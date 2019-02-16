d5ee3b37b6357a7168cf0b3b3d7b6885afdb3d89
Robert Cobb <rwcobbjr@gmail.com>
2019-02-16 16:08:12 +0000

    Automatically backed up by Learn

---
+++ first_method.rb
@@ -0,0 +1,46 @@
+def say_hello
+  puts "About to greet you"
+  puts "Hello world, four times"
+  puts "Just greeted you"
+end
+
+def say_hello_four_times
+  say_hello
+  say_hello
+  say_hello
+  say_hello
+end
+
+def times_four_thousand(mystery)
+  mystery * 4000
+end
+
+
+# create an array?
+colors = ["red", "yellow", "green"]
+
+
+# array methods
+# - put items into the array
+# push (<<) - puts an item at the end of the array
+# unshift - puts an item at the beginning of the array
+
+# - take them out
+# shift (take out from the front)
+# pop (take some item out from the back)
+
+# - read items from the array
+# access with  [] and an index (the position in the array)
+# indices start at 0
+
+# - how many items?
+# array.length - how many items
+
+# - what kinds of items are in the array?
+# - re-arrange the items
+# - unique things from an array
+
+
+
+
+
